2ab4e58cd98906da9a7fecd4b3e65b595c489b26
Gaurav Kumawat <148961465+Gaurav786kumawat@users.noreply.github.com>
2025-04-11 14:03:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,6 +69,10 @@ app.delete("/quotes/:id",(req,res)=>{
     res.redirect("/quotes");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000 🚀");
-}); 
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000 🚀");
+// }); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
